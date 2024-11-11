8321f2c33c3e22a0cce4f52b48e0f165c7957637
Romulo-de-Jesus <romulojf17@gmail.com>
2024-11-11 08:34:24 -0300

    jest

---
+++ swapi.spec.js
@@ -0,0 +1,6 @@
+// pode usar [it] ou [test/ para o nome do método]
+it ("1 + 1 deve ser igual a 2", () => {
+    //um teste bem simples 
+    const resultadoEsperado = 2
+    expect(1+1).toBe(resultadoEsperado)
+})
