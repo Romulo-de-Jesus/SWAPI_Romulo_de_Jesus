47ee6af04946819ef4e4fd97064cdcb552f2725b
Rômulo de Jesus Fernandes <romulojf17@gmail.com>
2024-11-13 02:55:10 -0300

    mais 3 testes

--- swapi.test.js
+++ swapi.test.js
@@ -11,5 +11,27 @@ describe('Testes da API SWAPI', () => {
     console.log('Dados da nave 9:', response.data)
   });
 
+  test('Buscar dados da nave 8', async () => {
+    const response = await axios.get(`${BASE_URL}/starships/8/`);
+    expect(response.status).toBe(200);
+    expect(response.data.name).toBeDefined();
 
+    console.log('Dados da nave 8:', response.data)
+  });
+
+  test('Buscar dados da nave 7', async () => {
+    const response = await axios.get(`${BASE_URL}/starships/7/`);
+    expect(response.status).toBe(200);
+    expect(response.data.name).toBeDefined();
+
+    console.log('Dados da nave 7:', response.data)
+  });
+
+  test('Buscar Lista de personagens 1', async () => {
+    const response = await axios.get(`${BASE_URL}/people/1/`);
+    expect(response.status).toBe(200);
+    expect(response.data.name).toBeDefined();
+
+    console.log('Dados da nave 8:', response.data)
+  });
 });
